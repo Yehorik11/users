51ba18cc8de439d82ff4d381709a1180458c2ad8
Yehor Ivashkin <ivashin.egor98@gmail.com>
2025-05-09 01:35:39 +0300

    fix problem with type property

--- src/models/User.js
+++ src/models/User.js
@@ -28,13 +28,21 @@ export class User {
   matches(query) {
     const q = query.toLowerCase();
     return (
+      this.id.toString().includes(q) ||
       this.name.toLowerCase().includes(q) ||
       this.username.toLowerCase().includes(q) ||
       this.email.toLowerCase().includes(q) ||
       this.phone.toLowerCase().includes(q) ||
       this.website.toLowerCase().includes(q) ||
+      this.address.street.toLowerCase().includes(q) ||
+      this.address.suite.toLowerCase().includes(q) ||
       this.address.city.toLowerCase().includes(q) ||
-      this.company.name.toLowerCase().includes(q)
+      this.address.zipcode.toLowerCase().includes(q) ||
+      this.address.geo.lat.toLowerCase().includes(q) ||
+      this.address.geo.lng.toLowerCase().includes(q) ||
+      this.company.name.toLowerCase().includes(q) ||
+      this.company.catchPhrase.toLowerCase().includes(q) ||
+      this.company.bs.toLowerCase().includes(q)
     );
   }
 }
