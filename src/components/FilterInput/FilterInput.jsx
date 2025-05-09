c4bf4f720e086a04dd755293dccf83286c321f2e
Yehor Ivashkin <ivashin.egor98@gmail.com>
2025-05-09 11:33:36 +0300

    fix text color in dark theme for input

--- src/components/FilterInput/FilterInput.jsx
+++ src/components/FilterInput/FilterInput.jsx
@@ -12,7 +12,7 @@ const FilterInput = ({ value, onChange }) => {
         value={value}
         onChange={handleFilter}
         placeholder='Type filter property'
-        className='border-2 border-gray-200 outline-none rounded-md px-[15px] py-[6px] text-[18px] w-full mb-6'
+        className='border-2 border-gray-200 dark:border-gray-600 outline-none rounded-md px-[15px] py-[6px] text-[18px] text-gray-800 dark:text-white placeholder-gray-400 dark:placeholder-gray-500 bg-white dark:bg-gray-800 w-full mb-6'
       />
     </div>
   );
